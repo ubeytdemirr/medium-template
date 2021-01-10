eff8ab27683ce5b252a610a244a691bdebd0a97d
Restyled.io <commits@restyled.io>
2021-01-10 15:11:48 +0000

    Restyled by clang-format

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
- 
+
+import './App.scss'
+
 import React from "react"
-import NavBar from "./components/NavBar/NavBar"
 import {BrowserRouter as Router, Route} from "react-router-dom"
+
+import NavBar from "./components/NavBar/NavBar"
 import Home from "./pages/home/Home";
 import NewStory from "./pages/new-story/NewStory"
 import Topics from './pages/topics/Topics'
-import './App.scss'
 
 function App() {
   return (
-    <Router>
-      <NavBar/>
-      <Route exact path="/" component={Home}/>
-      <Route exact path="/new-story" component={NewStory}/>
-      <Route exact path="/topics" component={Topics} />
-    </Router>
-  );
+      <Router><NavBar />
+      <Route exact path = "/" component =
+       { Home } />
+      <Route exact path="/new - story
+           " component={NewStory}/>
+       < Route exact path = "/topics" component = { Topics } />
+    </Router>);
 }
 
 export default App;
