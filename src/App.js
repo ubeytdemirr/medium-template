ba2fedd9c5c1781e3d7b083fc8e576fbf676b13d
Restyled.io <commits@restyled.io>
2021-01-10 15:12:49 +0000

    Restyled by prettier

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
+import "./App.scss";
 
-import './App.scss'
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-import React from "react"
-import {BrowserRouter as Router, Route} from "react-router-dom"
-
-import NavBar from "./components/NavBar/NavBar"
+import NavBar from "./components/NavBar/NavBar";
 import Home from "./pages/home/Home";
-import NewStory from "./pages/new-story/NewStory"
-import Topics from './pages/topics/Topics'
+import NewStory from "./pages/new-story/NewStory";
+import Topics from "./pages/topics/Topics";
 
 function App() {
   return (
-      <Router><NavBar />
-      <Route exact path = "/" component =
-       { Home } />
-      <Route exact path="/new - story
-           " component={NewStory}/>
-       < Route exact path = "/topics" component = { Topics } />
-    </Router>);
+    <Router>
+      <NavBar />
+      <Route exact path="/" component={Home} />
+      <Route
+        exact
+        path="/new - story
+           "
+        component={NewStory}
+      />
+      <Route exact path="/topics" component={Topics} />
+    </Router>
+  );
 }
 
 export default App;
