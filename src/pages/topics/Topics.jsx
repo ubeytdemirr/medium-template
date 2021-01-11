7408f9842d95af31e42de99a555e8a0ab3ab3b33
Luis Antonio Canettoli <hello@luisanton.io>
2021-01-11 17:44:09 +0100

    Update Topics.jsx

--- src/pages/topics/Topics.jsx
+++ src/pages/topics/Topics.jsx
@@ -15,42 +15,42 @@ export default class Topics extends React.Component {
           
         </Col>
       </Row>
-        {
-          Object.entries(topics).map(([topic, subs]) => (
-            <>
-              <Col xs={12}>
-                <div className="subject-wrapper">
-                  <div className="d-flex">
-                    <h4>{topic}</h4>
-                  </div>
+      {
+        Object.entries(topics).map(([topic, subs]) => (
+          <>
+            <Col xs={12}>
+              <div className="subject-wrapper">
+                <div className="d-flex">
+                  <h4>{topic}</h4>
                 </div>
-              </Col>
-              <Row className="px-5">
-              {
-                Object.entries(subs).map(([title, image]) => (
-                  <Col xs={12} md={6} lg={4} className="my-4 ">
-                    <Card>
-                      <Card.Body>
-                        <Card.Title className="topic-title">
-                          {title}
-                          <Button variant="outline-dark" className="rounded-pill">
-                            <span>
-                              <svg width="25" height="25">
-                                <path d="M20 12h-7V5h-1v7H5v1h7v7h1v-7h7" fill-rule="evenodd" />
-                              </svg>
-                            </span>
-                          </Button>
-                        </Card.Title>
-                      </Card.Body>
-                      <Card.Img variant="top" src={image} />
-                    </Card>
-                  </Col>
-                ))
-              }
-              </Row>
-            </>)
-          )
-        }
+              </div>
+            </Col>
+            <Row className="px-5">
+            {
+              Object.entries(subs).map(([title, image]) => (
+                <Col xs={12} md={6} lg={4} className="my-4 ">
+                  <Card>
+                    <Card.Body>
+                      <Card.Title className="topic-title">
+                        {title}
+                        <Button variant="outline-dark" className="rounded-pill">
+                          <span>
+                            <svg width="25" height="25">
+                              <path d="M20 12h-7V5h-1v7H5v1h7v7h1v-7h7" fill-rule="evenodd" />
+                            </svg>
+                          </span>
+                        </Button>
+                      </Card.Title>
+                    </Card.Body>
+                    <Card.Img variant="top" src={image} />
+                  </Card>
+                </Col>
+              ))
+            }
+            </Row>
+          </>)
+        )
+      }
     </Container>
   );
 }
