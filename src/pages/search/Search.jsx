5705c2c512391f93dc566aa495ed3497efc22e95
Luis Antonio Canettoli <hello@luisanton.io>
2021-01-11 17:02:17 +0100

    Search page done

--- src/pages/search/Search.jsx
+++ src/pages/search/Search.jsx
@@ -4,18 +4,37 @@ import ArticleListItem from "../../components/ArticleListItem/ArticleListItem";
 
 import './styles.scss'
 import articles from '../home/articles.json'
+import TagsList from '../../components/TagsList/TagsList';
 
 export default class Search extends React.Component {
+    state = {
+        query: "",
+        articles
+    }
+
+    queryDidChange = ({target}) => {
+        let query = target.value.toLocaleLowerCase()
+        this.setState({
+            query,
+            articles: query 
+                ? this.state.articles.filter( article => article.headLine.toLocaleLowerCase().includes(query) ) 
+                : articles
+        })
+    }
+
     render () {
         return <Container id="search-page">
             <Row>
-                <Col xs={12} md={4} className="order-md-1">
-                    Insert tags component here
+                <Col xs={12} >
+                    <input type="search" placeholder="Search..." value={this.state.query} onChange={this.queryDidChange}/>
+                </Col>
+
+                <Col xs={12} md={4} className="order-md-2">
+                    <TagsList />
                 </Col>
                 <Col xs={12} md={8}>
-                    <input type="search" placeholder="Search..." />
                     {
-                        articles.map( article => <ArticleListItem
+                        this.state.articles.map( article => <ArticleListItem
                             article={article}
                             articleImg={"top"}
                             headingFont={"large"}
